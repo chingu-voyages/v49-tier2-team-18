77405282c168e34084043dde15544b7cf93cb6a0
Brian Slaby <brian.slaby@gmail.com>
2024-05-23 11:49:18 -0400

    Issue #17 - [WIP] Groq AI API test request/response
    groq-api.js contains API Key placeholder, hard coded requestBody variable,
    and getGroqChatCompletion function.  The function currently console logs
    the message content from the first item in the response choices array.
    
    App.jsx contains temporary code surrounded by AI TESTING comments.
    The getGroqChatCompletion is simply imported and called.

--- color-picker-project/src/App.jsx
+++ color-picker-project/src/App.jsx
@@ -3,7 +3,10 @@ import Navbar from "./components/navbar/Navbar";
 
 
 // AI TESTING
+import { getGroqChatCompletion } from "./lib/groq-api";
 
+const response = await getGroqChatCompletion()
+// END AI TESTING
 
 const App = () => {
   return (
--- color-picker-project/src/lib/groq-api.js
+++ color-picker-project/src/lib/groq-api.js
@@ -1,5 +1,8 @@
-// TODO
-    // get new API Key; deleted after pushing to GitHub.
+
+
+// API KEY PLACEHOLDER
+    // Manually paste API key when testing locally, delete before committing/pushing.
+const apiKey = ""
 
 
 const requestBody = {
@@ -12,15 +15,14 @@ const requestBody = {
             role: "user",
             content: "What kind of outfit would look good with red shoes?  For example, what color shirt and pants."
         }
-    ]
-};
-
-// if successful try with llama3-8b-8192
+    ],
+    "model": "llama3-8b-8192"
+}
 
 // will eventually need to take parameters for request body, etc.
 async function getGroqChatCompletion() {
     try {
-        const response = await fetch("https://api.groq.com/v1/models/llama3-70b/generate", {
+        const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
             method: "POST",
             headers: {
                 'Authorization': `Bearer ${apiKey}`,
@@ -29,41 +31,12 @@ async function getGroqChatCompletion() {
             body: JSON.stringify(requestBody)
         })
         const data = await response.json()
-        console.log(data)
+        console.log(data.choices[0].message.content)
     }
     catch(error) {
         console.error(error)
-    }
-
-    
+    } 
 }
-
-
-
-
-// Chat Completion example from Groq docs
-
-// async function main() {
-//     const chatCompletion = await getGroqChatCompletion()
-//     // Print the completion returned by the LLM
-//     process.stdout.write(chatCompletion.choices[0]?.message?.content || "")
-// }
-
-// async function getGroqChatCompletion() {
-//     return groq.chat.completions.create({
-//         messages: [
-//             {
-//                 role: "system",
-//                 content: "You are a helpful assistant specializing in recommending harmonious color combinations for various purposes.  For example, outfit coordination, interior design, website design, graphic design projects, etc."
-//             },
-//             {
-//                 role: "user",
-//                 content: "What kind of outfit would look good with red shoes?  For example, what color shirt and pants."
-//             }
-//         ],
-//         model: "llama3-8b-8192"
-//     })
-// }
 
 export {
     getGroqChatCompletion
