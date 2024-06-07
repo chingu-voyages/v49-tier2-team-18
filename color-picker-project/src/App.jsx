9cd9feef6afb3483c4faa3d36420af00259a4f62
Brian Slaby <brian.slaby@gmail.com>
2024-06-06 21:04:43 -0400

    Issue #19 - Add formatResponseText function
    Added formatResponseText function in ResponseDisplay.jsx that takes
    the AI response string, splits it on the \n characters, and maps over
    the array with a break tag for each "" and a paragraph tag for each
    non-empty string.  This allows the response to render in a much more
    readable format.
    
    Also added a response header that renders above the response for
    clarity.  The response header was given a margin bottom of 1em in
    ResponseDisplay.css.
    
    Finally, the test code that console logged the AI response was deleted
    from App.jsx since the response is now rendered on the page.

--- color-picker-project/src/App.jsx
+++ color-picker-project/src/App.jsx
@@ -9,12 +9,6 @@ const App = () => {
   const [ aiResponse, setAiResponse ] = useState(null)
   const [ testColor, setTestColor ] = useState("#036ffc")
 
-  // TEST CODE >>>>>>
-  if (aiResponse) {
-    console.log(aiResponse)
-  }
-  
-  // <<<<<< TEST CODE
 
   return (
     <div>
--- color-picker-project/src/components/response-display/ResponseDisplay.jsx
+++ color-picker-project/src/components/response-display/ResponseDisplay.jsx
@@ -4,9 +4,34 @@ import "./ResponseDisplay.css"
 
 const ResponseDisplay = ({ aiResponse }) => {
 
+    function formatResponseText(response) {
+        return response.split("\n").map((line, index) => {
+            if (line === "" ) {
+                return <br key={index} />
+            } else {
+                return (
+                    <p key={index}>{line}</p>
+                )
+            }
+        })
+    }
+
     return (
         <div className="response-container">
-            {aiResponse}
+            {
+                !aiResponse &&
+                <p>Select a color, fill out the color details, and check back here for your personalized recommendation!</p>
+            }
+
+            {
+                aiResponse &&
+                <h3 className="response-header">Your Personalized Color Recommendation:</h3>
+            }
+
+            {
+                aiResponse &&
+                formatResponseText(aiResponse)
+            }
         </div>
     )
 }
