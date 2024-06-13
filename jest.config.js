85707a99b500f44507b7bd0d0bf4de860af40e2e
Jonathan <jwmp001@gmail.com>
2024-06-12 22:45:52 -0400

    Fixed an issue where the test suites were not covering jsx file types by updating the jest.config.js.

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,10 @@ const config = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
