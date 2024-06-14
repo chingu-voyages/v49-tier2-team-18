e918a009e66f6913d47447b44ee3070ec247f2f7
Brian Slaby <brian.slaby@gmail.com>
2024-06-13 20:50:45 -0400

    Issue #57 - Refine AI prompt
    systemContent and userContent both refined within groq-api.js to
    generate better AI responses.

--- color-picker-project/src/lib/groq-api.js
+++ color-picker-project/src/lib/groq-api.js
@@ -2,7 +2,7 @@
 
 // API KEY PLACEHOLDER
     // Manually paste API key when testing locally, delete before committing/pushing.
-const apiKey = ""
+const apiKey = "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"
 
 
 async function getGroqChatCompletion(
@@ -19,7 +19,7 @@ async function getGroqChatCompletion(
         ? `who recommends harmonious color combinations for various purposes` 
         : `who specializes in making harmonious color recommendations for ${context}`
 
-    const systemContent = `You are an enthusiastic assistant ${systemSpecialization}. You will be given a specific color in hexadecimal, RGB, HSL, or HSV format. You may also be given additional information that the user wants you to take into account. Your main job is to recommend colors that work well with the color provided by the user. You should take all information provided by the user into consideration.`
+    const systemContent = `You are an enthusiastic assistant ${systemSpecialization}. You will be given a specific color in hexadecimal, rgb, or hsl format. You may also be given additional information that the user wants you to take into account. Your main job is to recommend colors that work well with the color provided by the user. You should take all information provided by the user into consideration. For hsl codes, always include the "%" symbol after the numeric value for saturation and lightness. Do not use the optional alpha values in your color codes for the recommended color, even if the supplied color has an alpha value.`
 
     const userNumber = number === 1
         ? `${number} color that works well`
@@ -37,7 +37,7 @@ async function getGroqChatCompletion(
         ? `Here is some additional information: ${miscInfo}.`
         : ""
 
-    const userContent = `Please recommend ${userNumber} with ${color}. ${userColorScheme} ${userMood} ${userMiscInfo} Your color recommendations should be stated using ${format} color codes. Provide a brief explanation about your recommendation.`
+    const userContent = `Please recommend ${userNumber} with ${color}. ${userColorScheme} ${userMood} Your color recommendations should always be stated using ${format} color codes, in addition to any other type of color code requested. Provide a brief explanation about your recommendation. ${userMiscInfo}`
 
     const requestBody = {
         "messages": [
