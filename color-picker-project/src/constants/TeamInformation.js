32d6e11e4ad4b62d7d53ad6968aa9d65832f5cc2
TanutsaponJ <anakin112333@gmail.com>
2024-06-14 09:50:59 -0400

    Add profile pictures for team members

--- color-picker-project/src/constants/TeamInformation.js
+++ color-picker-project/src/constants/TeamInformation.js
@@ -1,9 +1,11 @@
+import ProfilePic1 from "../assets/TanProfile.png";
+import ProfilePic2 from "../assets/BrianProfileimg.png";
+
 export const TEAM_INFORMATION = [
   {
     name: "Tanutsapon Jinaongkan",
     Role: "Web Developer",
-    profileImg:
-      "https://private-user-images.githubusercontent.com/114305352/339075393-07548eb1-a0c0-4d15-a1c9-20b019700d44.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMTQ0MDAsIm5iZiI6MTcxODIxNDEwMCwicGF0aCI6Ii8xMTQzMDUzNTIvMzM5MDc1MzkzLTA3NTQ4ZWIxLWEwYzAtNGQxNS1hMWM5LTIwYjAxOTcwMGQ0NC5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNjEyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDYxMlQxNzQxNDBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kZmU1ZjcwZjA2MjkyMGMxOTUxYjUwNTZjOTM3ZGYxNTFiMWUyODIzMjVlYjIxZjdiMzNlZTgwZmFkNTgxNzY0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.QumsOY23TnaeccmDKBQ32zFix-qBJX1gIgnbjw2A6W4",
+    profileImg: ProfilePic1,
     LinkedInProfile: "https://www.linkedin.com/in/tanutsapon/",
     GitHubProfile: "https://github.com/TanutsaponJ",
     description:
@@ -12,8 +14,7 @@ export const TEAM_INFORMATION = [
   {
     name: "Brian Slaby",
     Role: "Web Developer",
-    profileImg:
-      "https://private-user-images.githubusercontent.com/106836794/338317795-399ec470-2676-4a75-b904-7d8de95b3bba.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMTI1NzEsIm5iZiI6MTcxODIxMjI3MSwicGF0aCI6Ii8xMDY4MzY3OTQvMzM4MzE3Nzk1LTM5OWVjNDcwLTI2NzYtNGE3NS1iOTA0LTdkOGRlOTViM2JiYS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNjEyJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDYxMlQxNzExMTFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT04OGRkZGEyMTcxZDgzMDdiMGY3MmQ0MjQ0OTI1NzQ5MzRiM2NiZmZlYzU4YWRkMzc4Y2ZkNDVlNjgwMjBkYmZmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.sQhJYn3ogdFR5Vs4eG5BVnFuwWzn5gaM2lmuRe-Tp3Q",
+    profileImg: ProfilePic2,
     LinkedInProfile: "https://www.linkedin.com/in/brian-slaby-78022388/",
     GitHubProfile: "https://github.com/BrianSlaby",
     description:
